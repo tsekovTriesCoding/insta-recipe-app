a0ca8e9113ee97cf9814b34db9c86fdf1d33e4a9
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-01-17 13:20:01 +0200

    add id field

--- src/main/java/app/web/dto/UserProfileInfo.java
+++ src/main/java/app/web/dto/UserProfileInfo.java
@@ -2,10 +2,12 @@ package app.web.dto;
 
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 public class UserProfileInfo {
+    private UUID id;
     private String username;
     private String email;
     private String profilePictureUrl;
-    private String password;
 }
