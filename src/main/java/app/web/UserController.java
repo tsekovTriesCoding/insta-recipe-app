0d5fbb808dc057bdf7e0a96e89ef220d35dca2f1
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-01-17 13:19:35 +0200

    change resources

--- src/main/java/app/web/UserController.java
+++ src/main/java/app/web/UserController.java
@@ -3,20 +3,16 @@ package app.web;
 import app.user.model.User;
 import app.user.service.UserService;
 import app.web.dto.RegisterRequest;
-import app.web.dto.UserProfileInfo;
-import app.web.dto.UserUpdate;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.io.IOException;
-import java.security.Principal;
 
 @RequiredArgsConstructor
 @Controller
@@ -60,28 +56,5 @@ public class UserController {
         User registeredUser = userService.register(registerRequest);
 
         return new ModelAndView("redirect:/home");
-    }
-
-    @GetMapping("/profile")
-    public ModelAndView getMyProfilePage(Principal principal) {
-        ModelAndView modelAndView = new ModelAndView("profile");
-
-        UserProfileInfo userProfileInfo = userService.getByUsername(principal.getName());
-
-        modelAndView.addObject("userProfileInfo", userProfileInfo);
-        return modelAndView;
-    }
-
-    @PutMapping("/profile")
-    public ModelAndView updateProfile(UserProfileInfo userProfileInfo,
-                                      @RequestParam("profilePicture") MultipartFile file,
-                                      Principal principal) throws IOException {
-        ModelAndView modelAndView = new ModelAndView("profile");
-
-        UserProfileInfo update = userService.update(userProfileInfo, file,principal.getName());
-
-        modelAndView.addObject("userProfileInfo", update);
-
-        return modelAndView;
     }
 }
