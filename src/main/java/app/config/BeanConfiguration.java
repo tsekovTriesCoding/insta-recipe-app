ae94a79399ecaa8fe1c6763b8ca2cc59425d22f4
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-02-10 17:25:36 +0200

    remove unwanted bean

--- src/main/java/app/config/BeanConfiguration.java
+++ src/main/java/app/config/BeanConfiguration.java
@@ -1,16 +1,10 @@
 package app.config;
 
-import app.category.repository.CategoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class BeanConfiguration {
@@ -19,24 +13,7 @@ public class BeanConfiguration {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    @Bean
-    public DataSourceInitializer dataSourceInitializer(DataSource dataSource,
-                                                       CategoryRepository categoryRepository,
-                                                       ResourceLoader resourceLoader) {
-
-        DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
-        dataSourceInitializer.setDataSource(dataSource);
-
-        if (categoryRepository.count() == 0) {
-            ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
-            resourceDatabasePopulator.addScript(resourceLoader.getResource("classpath:data.sql"));
-            dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
-        }
-
-        return dataSourceInitializer;
-    }
-
+    
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
