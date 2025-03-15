07b96d033532606950b1bab90eb2f6d0a0ddbdbe
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-03-15 17:24:41 +0200

    remove unused beans

--- src/main/java/app/config/BeanConfiguration.java
+++ src/main/java/app/config/BeanConfiguration.java
@@ -2,8 +2,6 @@ package app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
-import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -13,15 +11,5 @@ public class BeanConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SpelExpressionParser spelExpressionParser() {
-        return new SpelExpressionParser();
-    }
-
-    @Bean
-    public StandardEvaluationContext standardEvaluationContext() {
-        return new StandardEvaluationContext();
     }
 }
