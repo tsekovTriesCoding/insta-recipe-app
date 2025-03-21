9bc04979c7438eff4219e5fca935752595935450
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-03-21 09:12:41 +0200

    imports fix

--- src/test/java/app/recipe/RecipeServiceTest.java
+++ src/test/java/app/recipe/RecipeServiceTest.java
@@ -26,10 +26,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.awt.*;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.List;
