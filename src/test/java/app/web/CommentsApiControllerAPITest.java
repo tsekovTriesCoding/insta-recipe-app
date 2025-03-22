f0fc2a3146da6f48611d08d6b317e5abafc5c322
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-03-22 17:47:30 +0200

    rename

--- src/test/java/app/web/CommentsApiControllerAPITest.java
+++ src/test/java/app/web/CommentsApiControllerAPITest.java
@@ -28,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(CommentsApiController.class)
 @WithMockUser
-public class CommentsApiControllerIT {
+public class CommentsApiControllerAPITest {
 
     @MockitoBean
     private CommentService commentService;
